48509cab2f981f3706301d78ac7113393b60affc
Dishant Aggarwal <dishantaggarwal24@gmail.com>
2024-07-06 21:01:33 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AboutSection from './components/AboutSection';
+import SkillsSection from './components/SkillsSection';
+import ProjectsSection from './components/ProjectsSection';
+import ContactSection from './components/ContactSection';
+import FooterSection from './components/FooterSection';
+import GlobalStyle from './GlobalStyles';
+import NavbarPages from './components/NavbarPages';
+import NavBurgerMenu from './components/NavBurgerMenu'
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <AnimatedCursor
+        innerSize={12}
+        outerSize={12}
+        color='128, 0, 128'
+        outerAlpha={0.2}
+        innerScale={1.5}
+        outerScale={5}
+      />
+      <NavbarPages />
+      <NavBurgerMenu />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+      <FooterSection />
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,58 @@
+const contentCard = [
+    {
+        id: 1,
+        title: 'HeavenHub1',
+        body: 'It is a full-stack hotel booking web application using the MERN stack with JWT authentication for secure user access. It containes integrated features include hotel search, an admin dashboard for management, and a responsive, efficient user experience.',
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1720266791/portfolio/heaven-hub_tg38ep.gif',
+        tech: `REACT REDUX NODEJS EXPRESSJS MONGODB REST APIs GIT`,
+        linkgithub: 'https://github.com/dishantagg24/hotel-reservation-frontend',
+        linkdemo: 'https://hotel-reservation-frontend-pearl.vercel.app/'
+    },
+    {
+        id: 2,
+        title: 'Country Flags',
+        body: 'Developed a responsive homepage displaying country flags, names, and details, adaptable to desktop and mobile, with region filtering and search features. Added detailed views for countries and a light/dark mode toggle for enhanced user experience.',
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1720267121/portfolio/country-flags_ttuyzc.gif',
+        tech: `REACT JAVASCRIPT SASS APIs`,
+        linkgithub: 'https://github.com/dishantagg24/flags-countries-react',
+        linkdemo: 'https://flags-countries-react.vercel.app/'
+    },
+    {
+        id: 3,
+        title: 'Project Management',
+        body: 'Developed a React web application for efficient project and task management, featuring capabilities to create and delete projects and manage associated task lists.',
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1720269142/portfolio/project-management_qeshq5.gif',
+        tech: `REACT JAVASCRIPT TAILWIND CSS`,
+        linkgithub: 'https://github.com/dishantagg24/project-management',
+        linkdemo: 'https://delicate-conkies-e0f2bc.netlify.app/'
+    },
+    {
+        id: 4,
+        title: 'Video Display',
+        body: "A web app similar to youtube built using react. It fetches data from the Youtube API and also show a title and description of the video at the bottom as well.",
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1640890915/portfolio/video-display_grwagh.gif',
+        tech: `REACT  SASS  AXIOS`,
+        linkgithub: 'https://github.com/dishantagg24/video-display',
+        linkdemo: 'https://video-display.netlify.app/'
+    },
+    {
+        id: 5,
+        title: 'RGB Color Game',
+        body: "A responsive, functional web app/game built with HTML, CSS and vanilla Javascript. The user is provided with 3 levels easy, medium and hard to choose from.",
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1640890734/portfolio/rgb-color-game_lrfieu.gif',
+        tech: `HTML  CSS  JAVASCRIPT`,
+        linkgithub: 'https://github.com/dishantagg24/RGB-Color-Game',
+        linkdemo: 'https://great-rgb-color-game24.herokuapp.com/'
+    },
+    {
+        id: 6,
+        title: 'Tindog Site',
+        body: "A beautiful startup landing page built using Bootstrap and more advanced CSS. It contains title section, features section, pricing plan section and much more...",
+        image: 'https://res.cloudinary.com/dg0oew5d9/image/upload/v1640890736/portfolio/tindog-site_qwtrsq.gif',
+        tech: `HTML  CSS  BOOTSTRAP`,
+        linkgithub: 'https://github.com/dishantagg24/Tindog-master-site',
+        linkdemo: 'https://dishantagg24.github.io/Tindog-master-site/'
+    },
+]
+
+export default contentCard
